14dd9f2892d46a275fe7fd26afbfbb874fc6ce7c
Tingting Zhan <tingtingzhan@gmail.com>
2026-02-10 22:54:17 -0500

    tzh

--- R/bibs.R
+++ R/bibs.R
@@ -1,15 +1,17 @@
 
 #' @title bibs of package psych.tzh
 #' 
+#' @param key,... \link[utils]{bibentry}
+#' 
 #' @keywords internal
-#' @importFrom utils bibentry
+#' @importFrom utils bibentry person
 #' @name psych_bib
 #' @export
-.gorsuch83 <- \() {
+.gorsuch83 <- \(key = 'Gorsuch83', ...) {
   bibentry(
-    bibtype = 'book', key = 'Gorsuch83', 
+    bibtype = 'book', key = key, ...,
     title = 'Factor Analysis',
-    author = 'Richard L. Gorsuch',
+    author = person(given = c('Richard', 'L.'), family = 'Gorsuch'),
     year = '1983',
     edition = '2nd',
     publisher = 'Psychology Press',
